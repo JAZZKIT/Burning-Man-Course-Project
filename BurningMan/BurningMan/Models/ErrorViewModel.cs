2b457b199ebf7d3dd0fe31a7ba8afda6b307247c
Denis Neshcheretnyi <jazzkit.f@gmail.com>
2020-12-09 20:06:50 +0200

    HTML+CSS

---
+++ BurningMan/BurningMan/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BurningMan.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
