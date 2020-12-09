5b1c16ed809ca47365b8e23044ca386130779b95
Denis Neshcheretnyi <jazzkit.f@gmail.com>
2020-12-09 22:04:00 +0200

    Change some HTML

---
+++ BurningMan/BurningMan/Controllers/Registration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurningMan.Controllers
+{
+    public class Registration : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
