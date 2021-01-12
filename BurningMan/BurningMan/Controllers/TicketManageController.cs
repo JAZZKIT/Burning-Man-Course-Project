2c3e1b9adc05cfdd6aed93d79d8d9097e847a7a8
Denis Neshcheretnyi <jazzkit.f@gmail.com>
2021-01-12 23:06:29 +0200

    Tickets Buy System was started

---
+++ BurningMan/BurningMan/Controllers/TicketManageController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurningMan.Controllers
+{
+    public class TicketManageController : Controller
+    {
+        public IActionResult TicketMainPage()
+        {
+            return View();
+        }
+    }
+}
